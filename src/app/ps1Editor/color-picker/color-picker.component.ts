99d4c1601c885fbd3745cf28339fe58fe2b0ced3
CanadianCommander <ben.benetti@shaw.ca>
2019-06-07 20:11:24 -0700

    Fix color picker overflow. + color picker now re-positions if it would go off the page.

--- src/app/ps1Editor/color-picker/color-picker.component.ts
+++ src/app/ps1Editor/color-picker/color-picker.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 import { Ps1Element } from "../model/ps1-element"
 
 @Component({
@@ -7,17 +7,27 @@ import { Ps1Element } from "../model/ps1-element"
   styleUrls: ['./color-picker.component.scss']
 })
 export class ColorPickerComponent implements OnInit {
-
-  @Input() colorList: string[]
+  @ViewChild('colorPickerContainer') container: ElementRef;
+  @Input() colorList: string[];
   @Input() ps1Element: Ps1Element;
+  xOffset: number = 0;
 
   constructor() { }
 
   ngOnInit() {
+    this.ensureVisible();
   }
 
   setColor(event) {
     this.ps1Element.color.setCurrentColorIndex(<number>event.target.attributes["data-color-index"].value);
   }
 
+  // ensure color picker is visible by adjusting position
+  private ensureVisible(): void {
+    let pos = $(this.container.nativeElement).position();
+    let width = $(this.container.nativeElement).width();
+    if (pos.left + width > $('body').width()) {
+      this.xOffset = $('body').width() - (pos.left + width) - 10;
+    }
+  }
 }
